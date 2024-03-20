db01b699c27d3db7b30b25a552ea461ed8bf6c7d
Praveena Muvva <praveenamuvva@Praveenas-Air.lan>
2024-03-20 13:19:03 -0700

    included array and arraylist methods

--- JAVA/src/main/java/DataStructures/LearnArrayList.java
+++ JAVA/src/main/java/DataStructures/LearnArrayList.java
@@ -1,6 +1,7 @@
 package DataStructures;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Iterator;
 
 public class LearnArrayList {
@@ -8,23 +9,36 @@ public class LearnArrayList {
         ArrayList<String> students = new ArrayList<>();
         students.add("Praveena");
         students.add("Veena");
-        System.out.println(students);
         students.add(1, "AAAAA"); // Adds at a specific index
-        System.out.println(students); //toString method will be invoked whenever we are trying to access these
-        System.out.println(students.get(2));
+        //students.addAll(Comparator); // need to use comparator
+
+        students.get(2);
+        students.indexOf("e"); //returns the first occurance of the element
+        students.lastIndexOf("e"); // returns the last occurance of the element
+        students.subList(2, 4); // returns the list between from and to index positions
+        students.set(0, "BBBB"); // modify element at any specific index
+
         students.remove(String.valueOf("AAAAA")); // can be an index or an element of that type / object
-        System.out.println(students);
-        System.out.println(students.contains("Veena"));
+        students.remove(0); // removes element at an index
+
+        students.isEmpty(); // returns true or false
+        students.size(); // Returns the size of the arraylist
+        students.contains("Veena"); // returns true or false
 
         //Iteration
         for(String s: students) {
             System.out.println("Hello " + s);
         }
 
-
-        //Iterator. We can iterator in any of the collections
+        // Iterator. We can iterator in any of the collections
         Iterator<String> it = students.iterator();
         while (it.hasNext())
             System.out.println("Hi " + it.next());
+
+        //Sorting
+        Collections.sort(students); // pass comparator if required
+
+        students.toArray(); // converting to array
+        students.clear(); // removes all the elements
     }
 }
--- JAVA/src/main/java/DataStructures/LearnArraysClass.java
+++ JAVA/src/main/java/DataStructures/LearnArraysClass.java
@@ -2,16 +2,22 @@ package DataStructures;
 
 import java.util.Arrays;
 
-public class LearnArrays {
+public class LearnArraysClass {
     public static void main(String[] args) {
         //binary search can be done directly with inbuilt method binarySearch
-        Integer[] numbers = {3, 2, 7, 5, 3, 4, 9};
+        int[] numbers = {3, 2, 7, 5, 3, 4, 9};
+        int[] values = {55, 66, 77, 88};
+
+        Arrays.binarySearch(numbers, 5); // returns the index. Can also provide from and to index params
+        Arrays.compare(numbers, values); // returns 0 or 1
+
         Arrays.sort(numbers);
 
         for(int i=0; i<numbers.length; i++)
             System.out.println(numbers[i]);
         Arrays.fill(numbers, 12);
 
+        Arrays.asList(numbers); // coverts array to list
         // toList doesn't work on char arrays
         // Stringbuilder reverse methods, SB to array method
         //8192 limit crossed - parallel sort
